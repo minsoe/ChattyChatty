c5e4e7ab3f57bfd608a338d05e326a9d658d4d3f
minsoe <mszhawk@gmail.com>
2024-09-26 00:51:36 +0800

    added integration test

--- tests/openai_service_tests.py
+++ tests/openai_service_tests.py
@@ -1,14 +1,23 @@
 import unittest
 from unittest.mock import patch, MagicMock, AsyncMock
+
+from dotenv import load_dotenv
+
+from app.database import init_mongodb
 from app.openai_service import OpenAIService
 from app.models import Conversation
 
 class OpenAIServieTests(unittest.TestCase):
 
+    def _test_actual_message(self):
+        load_dotenv()
+        init_mongodb()
+        service = OpenAIService()
+        self.assertIsNotNone(service.send(prompt="testing"))
+
     @patch('app.models.Conversation')
-    @patch('app.database.find_conversation_by_id')
     @patch('app.openai_service.OpenAI')
-    def test_send_message(self, mock_ai, mock_find, mock_conversation):
+    def test_send_message(self, mock_ai, mock_conversation):
         mocked_message = "Mocked response"
         mock_response = MagicMock()
         mock_response.choices = [MagicMock(message=mocked_message)]
@@ -18,10 +27,9 @@ class OpenAIServieTests(unittest.TestCase):
 
         mock_conversation_instance = mock_conversation.get.return_value
         mock_conversation_instance.save = AsyncMock()
-
-
         service = OpenAIService()
         self.assertEqual(service.send(prompt="Test"), mocked_message)
+        mock_conversation_instance.save.assert_called_once()
 
 
 
